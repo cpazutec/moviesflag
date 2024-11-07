87d8e8953a9e59df5b588cc702af34e864f79f2e
= <cpaz@utec.edu.pe>
2024-11-06 21:44:18 -0500

    c

--- app.py
+++ app.py
@@ -1,21 +1,18 @@
 from flask import Flask, render_template, request, jsonify
 import requests
 import json
+import functools
+import timeit
+from concurrent.futures import ThreadPoolExecutor
+import asyncio
+from timeit import default_timer
+
 
 app = Flask(__name__)
-apikey = "<apikey>"
+apikey = "a2c21c91"
 
-def searchfilms(search_text):
-    url = "https://www.omdbapi.com/?s=" + search_text + "&apikey=" + apikey
-    response = requests.get(url)
-    if response.status_code == 200:
-        return response.json()
-    else:
-        print("Failed to retrieve search results.")
-        return None
-    
-def getmoviedetails(movie):
-    url = "https://www.omdbapi.com/?i=" + movie["imdbID"] + "&apikey=" + apikey
+def searchfilms(search_text, page):
+    url = f"https://www.omdbapi.com/?type=movie&page={page}&s={search_text}&apikey={apikey}"
     response = requests.get(url)
     if response.status_code == 200:
         return response.json()
@@ -23,30 +20,56 @@ def getmoviedetails(movie):
         print("Failed to retrieve search results.")
         return None
 
+@functools.cache
+def getmoviedetails(movieid):
+    url = "https://www.omdbapi.com/?i=" + movieid + "&apikey=" + apikey
+    response = requests.get(url)
+    if response.status_code == 200:
+        return response.json()
+    else:
+        print("Failed to retrieve search results.")
+        return None
+
+@functools.cache
 def get_country_flag(fullname):
 
     url = f"https://restcountries.com/v3.1/name/{fullname}?fullText=true"
+    print(f"call {url}")
     response = requests.get(url)
     if response.status_code == 200:
         country_data = response.json()
         if country_data:
-            return country_data[0].get("flags", {}).get("svg", None)
+            return {
+                "name": fullname,
+                "flag": country_data[0].get("flags", {}).get("svg", None)
+            }
     print(f"Failed to retrieve flag for country code: {fullname}")
-    return None
+    return {
+                "name": fullname,
+                "flag": None
+            }
 
-def merge_data_with_flags(filter):
-    filmssearch = searchfilms(filter)
+def merge_data_with_flags(filter, parallel):
+    filmssearch = searchfilms(filter, 1)
+    filmssearchres = filmssearch["Search"]
+    if len(filmssearchres) == 10:
+        secondpage = searchfilms(filter, 2)["Search"]
+        for movie2 in secondpage:
+            filmssearchres.append(movie2)
     moviesdetailswithflags = []
-    for movie in filmssearch["Search"]:
-         moviedetails = getmoviedetails(movie)
+    for movie in filmssearchres:
+         moviedetails = getmoviedetails(movie["imdbID"])
          countriesNames = moviedetails["Country"].split(",")
          countries = []
-         for country in countriesNames:
-            countrywithflag = {
-                "name": country.strip(),
-                "flag": get_country_flag(country.strip())
-            }
-            countries.append(countrywithflag)
+         if parallel=="1":
+            t = []
+            for country in countriesNames:
+                t.append(country.strip())
+            with ThreadPoolExecutor(max_workers=10) as executor:
+                countries = list(executor.map(get_country_flag, t))
+         if parallel == "0":
+            for country in countriesNames:
+                countries.append(get_country_flag(country.strip()))
          moviewithflags = {
             "title": moviedetails["Title"],
             "year": moviedetails["Year"],
@@ -58,14 +81,21 @@ def merge_data_with_flags(filter):
 
 @app.route("/")
 def index():
-    filter = request.args.get("filter", "").upper()
-    return render_template("index.html", movies = merge_data_with_flags(filter))
+    filter = request.args.get("filter", "")
+    parallel = request.args.get("parallel", 1)
+    start = default_timer()
+    movies = merge_data_with_flags(filter, parallel)
+    end = default_timer()
+    print(end - start)
+    return render_template("index.html", movies = movies)
+    
 
 @app.route("/api/movies")
 def api_movies():
     filter = request.args.get("filter", "")
-    return jsonify(merge_data_with_flags(filter))    
+    parallel = request.args.get("parallel", "1")
+    return jsonify(merge_data_with_flags(filter, parallel))    
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True  , port=3000)
 
